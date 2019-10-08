4e788e084febfd9cdc2986b54c2d9a50924de20d
jiangbo2015 <772051431@qq.com>
2019-10-08 14:56:11 +0800

    hook

--- hook.js
+++ hook.js
@@ -3,5 +3,5 @@ const exec = require("child_process").execSync
 module.exports = (req, res) => {
 	exec("git pull && yarn")
 	console.log("receive post")
-	res.end("Welcome to Micro")
+	res.end("Welcome to Mic")
 }
