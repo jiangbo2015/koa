19bd3e5d20aa8a2c3bf03ae366af40bfa745e57e
jiangbo2015 <772051431@qq.com>
2019-10-09 09:28:47 +0800

    envent

--- hook.js
+++ hook.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
 	try {
 		const data = await json(res)
 		console.log(data)
-		console.log(data.body)
+		console.log(data.body, data.event)
 		exec("git pull && yarn")
 		// exec("cd ../koa-prod && git pull && yarn")
 		console.log(
