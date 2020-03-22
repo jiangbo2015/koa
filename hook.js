51d8ff1bc190d144cf73106a583f5a3625a29db2
jiangbo <772051431@qq.com>
2020-03-22 22:31:24 +0800

    hook

--- hook.js
+++ hook.js
@@ -7,16 +7,34 @@ const webhooks = new WebhooksApi({
 })
 
 webhooks.on("*", ({ id, name, payload }) => {
-	let pullrestart = "git pull && yarn && pm2 restart koa-test"
-	let pullrestartProd = "git pull && yarn && pm2 restart koa-prod"
+	let koaTest = "git pull && yarn && pm2 restart koa-test"
+	let crm = "cd ../mm-crm && git pull && yarn && pm2 restart crm"
+	let miss = "cd ../mm-next && git pull && yarn && pm2 restart miss"
+	let koaProd = "git pull && yarn && pm2 restart koa-prod"
 	console.log(payload.ref, "ref")
 	console.log(id, name)
 	// master 分支
 	try {
-		if (payload.ref.includes("master")) {
-			exec(`cd ../koa-prod && ${pullrestartProd}`)
-		} else {
-			exec(pullrestart)
+		if (payload.repository.name === "koa") {
+			if (payload.ref.includes("master")) {
+				exec(`cd ../koa-prod && ${koaProd}`)
+			} else {
+				exec(koaTest)
+			}
+		}
+		if (payload.repository.name === "mm-crm") {
+			if (payload.ref.includes("master")) {
+				exec(crm)
+			} else {
+				exec(crm)
+			}
+		}
+		if (payload.repository.name === "mm-next") {
+			if (payload.ref.includes("master")) {
+				exec(miss)
+			} else {
+				exec(miss)
+			}
 		}
 	} catch (err) {
 		console.log(err)
