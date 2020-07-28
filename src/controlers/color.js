619e1614c9914d38a112a7fbccf29013beba82a1
wyp10386 <yunpei.wang@uisee.com>
2020-07-28 13:09:37 +0800

    fix color获取时添加categoryId条件

--- src/controlers/color.js
+++ src/controlers/color.js
@@ -1,80 +1,85 @@
-import Color from "../models/color"
-import { response } from "../utils"
-import moment from "moment"
+import Color from "../models/color";
+import { response } from "../utils";
+import moment from "moment";
 
 const codePrefix = {
-	0: "S-",
-	1: "H-",
-}
+  0: "S-",
+  1: "H-",
+};
 
 export const add = async (ctx, next) => {
-	try {
-		const { type, code, value, ...others } = ctx.request.body
-		// const code = codePrefix[type] + moment().format("YYMMDDHHMMss")
-		let color = new Color({
-			type,
-			value,
-			code,
-			...others,
-		})
-		let data = await color.save()
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { type, code, value, ...others } = ctx.request.body;
+    // const code = codePrefix[type] + moment().format("YYMMDDHHMMss")
+    let color = new Color({
+      type,
+      value,
+      code,
+      ...others,
+    });
+    let data = await color.save();
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const getList = async (ctx, next) => {
-	try {
-		let { type, code, page = 1, limit = 20 } = ctx.request.query
+  try {
+    let { type, code, page = 1, limit = 20, categoryId } = ctx.request.query;
 
-		let q = {}
-		if (typeof code !== "undefined") {
-			q.code = {
-				$regex: new RegExp(code, "i"),
-			}
-		}
-		if (typeof type !== "undefined") {
-			q.type = type
-		}
-		let data = await Color.paginate(q, {
-			page,
-			// 如果没有limit字段，不分页
-			// limit: limit ? limit : 10000,
-			limit: parseInt(limit),
-			sort: {
-				createTime: -1,
-			},
-		})
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+    let q = {};
+    if (typeof code !== "undefined") {
+      q.code = {
+        $regex: new RegExp(code, "i"),
+      };
+    }
+    if (typeof type !== "undefined") {
+      q.type = type;
+    }
+    if (typeof type !== "undefined") {
+      q.categoryId = {
+        $in: [categoryId],
+      };
+    }
+    let data = await Color.paginate(q, {
+      page,
+      // 如果没有limit字段，不分页
+      // limit: limit ? limit : 10000,
+      limit: parseInt(limit),
+      sort: {
+        createTime: -1,
+      },
+    });
+    ctx.body = response(true, { ...data, categoryId }, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const update = async (ctx, next) => {
-	try {
-		const { _id, ...others } = ctx.request.body
-		let data = await Color.findByIdAndUpdate(
-			{ _id },
-			{ ...others },
-			{ new: true }
-		)
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { _id, ...others } = ctx.request.body;
+    let data = await Color.findByIdAndUpdate(
+      { _id },
+      { ...others },
+      { new: true }
+    );
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const del = async (ctx, next) => {
-	try {
-		const { _id } = ctx.request.body
-		let data = await Color.findByIdAndRemove({ _id })
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { _id } = ctx.request.body;
+    let data = await Color.findByIdAndRemove({ _id });
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
