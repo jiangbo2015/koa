b7b261628bd98f69baebb7afec744d9e797990a1
wyp10386 <yunpei.wang@uisee.com>
2020-07-28 11:15:42 +0800

    fix 客户获取款式

--- src/controlers/style.js
+++ src/controlers/style.js
@@ -1,142 +1,160 @@
-import Style from "../models/style"
-import Channel from "../models/channel"
-import Goods from "../models/goods"
-import { response } from "../utils"
-import { getCurrentUser } from "./user"
+import Style from "../models/style";
+import Channel from "../models/channel";
+import Goods from "../models/goods";
+import { response } from "../utils";
+import { getCurrentUser } from "./user";
 
 export const add = async (ctx, next) => {
-	try {
-		const { body } = ctx.request
-		let style = new Style(body)
-		let data = await style.save()
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { body } = ctx.request;
+    let style = new Style(body);
+    let data = await style.save();
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const getList = async (ctx, next) => {
-	try {
-		let { tag, styleNo } = ctx.request.query
-		const currentUser = await getCurrentUser(ctx)
-		let styleIds = []
-		let data = []
-		let q = {}
-		if (tag) {
-			q = {
-				tags: {
-					$in: [tag],
-				},
-			}
-		}
-		if (styleNo) {
-			q.styleNo = styleNo
-		}
+  try {
+    let { tag, styleNo } = ctx.request.query;
+    const currentUser = await getCurrentUser(ctx);
+    let styleIds = [];
+    let data = [];
+    let q = {};
+    if (tag) {
+      q = {
+        tags: {
+          $in: [tag],
+        },
+      };
+    }
+    if (styleNo) {
+      q.styleNo = styleNo;
+    }
 
-		if (currentUser.role === 3) {
-			let channel = await Channel.findById({ _id: currentUser.channels[0] })
-			channel.styles.map((x) => styleIds.push(x.styleId))
-			data = await Style.find({
-				_id: {
-					$in: styleIds,
-				},
-				isDel: 0,
-				...q,
-			})
-		} else {
-			data = await Style.find({
-				isDel: 0,
-				...q,
-			})
-		}
+    if (currentUser.role === 3) {
+      let channel = await Channel.findById({ _id: currentUser.channels[0] });
+      channel.styles.map((x) => styleIds.push(x.styleId));
+      data = await Style.find({
+        _id: {
+          $in: styleIds,
+        },
+        isDel: 0,
+        ...q,
+      });
+    } else {
+      data = await Style.find({
+        isDel: 0,
+        ...q,
+      });
+    }
 
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		ctx.body = response(false, null, err.message)
-	}
-}
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const getUserStyleList = async (ctx, next) => {
-	try {
-		console.log("-----cids-----", _id)
-		let { tag, styleNo, _id } = ctx.request.query
-		let goodData = await Goods.findById({ _id }).lean()
-		const currentUser = await getCurrentUser(ctx)
-		console.log("-----cids-----")
-		let styleIds = []
-		let data = []
-		let q = {}
-		if (tag) {
-			q.tags = {
-				$in: [tag],
-			}
-		}
+  try {
+    console.log("-----cids-----", _id);
+    let { tag, styleNo, _id } = ctx.request.query;
+    let goodData = await Goods.findById({ _id }).lean();
+    const currentUser = await getCurrentUser(ctx);
+    console.log("-----cids-----");
+    let styleIds = [];
+    let data = [];
+    let q = {};
+    if (tag) {
+      q.tags = {
+        $in: [tag],
+      };
+    }
 
-		if (styleNo) {
-			q.styleNo = styleNo
-		}
+    if (styleNo) {
+      q.styleNo = styleNo;
+    }
 
-		// if (currentUser.role === 3) {
-		// 	let channel = await Channel.findById({ _id: currentUser.channels[0] })
-		// 	channel.styles.map((x) => styleIds.push(x.styleId))
+    let cids = [];
+    goodData.category.map((c) => {
+      console.log("c._id", c._id);
+      cids.push(c._id.toString());
+    });
 
-		// 	data = await Style.find({
-		// 		_id: {
-		// 			$in: styleIds,
-		// 		},
-		// 		isDel: 0,
-		// 		...q,
-		// 	})
-		// } else {
-		// 	data = await Style.find({
-		// 		isDel: 0,
-		// 		...q,
-		// 		"goods.goodId": { $in: [goodId] },
-		// 	})
-		// }
-		let cids = []
-		goodData.category.map((c) => {
-			console.log("c._id", c._id)
-			cids.push(c._id)
-		})
+    let categoryData = [];
 
-		console.log("-----cids-----", cids)
+    if (currentUser.role === 3) {
+      let channel = await Channel.findById({ _id: currentUser.channels[0] });
+      channel.styles.map((x) => styleIds.push(x.styleId));
 
-		let categoryData = []
-		for (let i = 0; i < goodData.category.length; i++) {
-			let c = goodData.category[i]
-			let styles = await Style.find({
-				isDel: 0,
-				...q,
-				categoryId: { $in: [c._id.toString()] },
-			})
-			categoryData.push({
-				name: c.name,
-				_id: c._id,
-				styles: styles,
-			})
-		}
+      let match = {
+        _id: {
+          $in: styleIds,
+        },
+        categoryId: {
+          $in: cids,
+        },
+        isDel: 0,
+        ...q,
+      };
 
-		ctx.body = response(true, { category: categoryData }, "成功")
-	} catch (err) {
-		ctx.body = response(false, null, err.message)
-	}
-}
+      let styles = await Style.aggregate([
+        {
+          $match: match,
+        },
+
+        {
+          $unwind: "$categoryId",
+        },
+        {
+          $group: {
+            _id: "$categoryId",
+            styles: {
+              $push: "$$ROOT",
+            },
+          },
+        },
+      ]);
+      categoryData.push({
+        styles,
+      });
+    } else {
+      for (let i = 0; i < goodData.category.length; i++) {
+        let c = goodData.category[i];
+        let styles = await Style.find({
+          isDel: 0,
+          ...q,
+          categoryId: { $in: [c._id.toString()] },
+        });
+        categoryData.push({
+          name: c.name,
+          _id: c._id,
+          styles: styles,
+        });
+      }
+    }
+
+    ctx.body = response(true, { category: categoryData }, "成功");
+  } catch (err) {
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const update = async (ctx, next) => {
-	try {
-		const { _id, ...others } = ctx.request.body
-		let data = await Style.findByIdAndUpdate(
-			{ _id },
-			{ ...others },
-			{ new: true }
-		)
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { _id, ...others } = ctx.request.body;
+    let data = await Style.findByIdAndUpdate(
+      { _id },
+      { ...others },
+      { new: true }
+    );
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 /**
  *
@@ -146,29 +164,29 @@ export const update = async (ctx, next) => {
  * @param {object} target 要替换或添加的对象
  */
 const updateInnerArray = (res, field, key, target) => {
-	let index = res[field].findIndex((x) => x[key] === target[key])
-	if (index > -1) {
-		res[field].splice(index, 1, target)
-	} else {
-		res[field].push(target)
-	}
-}
+  let index = res[field].findIndex((x) => x[key] === target[key]);
+  if (index > -1) {
+    res[field].splice(index, 1, target);
+  } else {
+    res[field].push(target);
+  }
+};
 
 export const updateAttr = async (ctx, next) => {
-	try {
-		const { _id, ...attr } = ctx.request.body
-		let res = await Style.findById({
-			_id,
-		})
+  try {
+    const { _id, ...attr } = ctx.request.body;
+    let res = await Style.findById({
+      _id,
+    });
 
-		updateInnerArray(res, "attrs", "colorId", attr)
-		let data = await res.save()
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+    updateInnerArray(res, "attrs", "colorId", attr);
+    let data = await res.save();
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 /**
  * 给当前款式下，对通道分配尺寸，素色，花色
@@ -176,73 +194,73 @@ export const updateAttr = async (ctx, next) => {
  * 若有，则更新
  */
 export const assign = async (ctx, next) => {
-	try {
-		const { _id, ...channel } = ctx.request.body
+  try {
+    const { _id, ...channel } = ctx.request.body;
 
-		let res = await Style.findOneAndUpdate({
-			_id,
-			"channels.channelId": channel.channelId,
-		})
-		updateInnerArray(res, "channels", "channelId", channel)
+    let res = await Style.findOneAndUpdate({
+      _id,
+      "channels.channelId": channel.channelId,
+    });
+    updateInnerArray(res, "channels", "channelId", channel);
 
-		let data = await res.save()
+    let data = await res.save();
 
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const updateMany = async (ctx, next) => {
-	try {
-		const body = ctx.request.body
-		let data = await Style.updateMany(
-			{},
-			{
-				...body,
-			}
-		)
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const body = ctx.request.body;
+    let data = await Style.updateMany(
+      {},
+      {
+        ...body,
+      }
+    );
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const del = async (ctx, next) => {
-	try {
-		const { _id } = ctx.request.body
-		let data = await Style.findByIdAndUpdate({ _id }, { isDel: 1 })
-		ctx.body = response(true, data, "成功")
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+  try {
+    const { _id } = ctx.request.body;
+    let data = await Style.findByIdAndUpdate({ _id }, { isDel: 1 });
+    ctx.body = response(true, data, "成功");
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
 
 export const detail = async (ctx, next) => {
-	try {
-		const { _id, channelId } = ctx.request.query
-		// const { role } = await getCurrentUser(ctx, next)
+  try {
+    const { _id, channelId } = ctx.request.query;
+    // const { role } = await getCurrentUser(ctx, next)
 
-		let data = await await Style.findById(_id)
-			// .populate("goodsId")
-			.populate({
-				path: "plainColors.colorId",
-			})
-			.populate({
-				path: "flowerColors.colorId",
-			})
-			.populate("size")
-			.select("-plainColors._id -flowerColors._id")
-			.exec()
+    let data = await await Style.findById(_id)
+      // .populate("goodsId")
+      .populate({
+        path: "plainColors.colorId",
+      })
+      .populate({
+        path: "flowerColors.colorId",
+      })
+      .populate("size")
+      .select("-plainColors._id -flowerColors._id")
+      .exec();
 
-		ctx.body = response(true, data, "成功")
+    ctx.body = response(true, data, "成功");
 
-		// data = JSON.parse(JSON.stringify(data))
-	} catch (err) {
-		console.log(err)
-		ctx.body = response(false, null, err.message)
-	}
-}
+    // data = JSON.parse(JSON.stringify(data))
+  } catch (err) {
+    console.log(err);
+    ctx.body = response(false, null, err.message);
+  }
+};
