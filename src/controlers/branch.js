4f5b521c5d97fac9be8326329aa888a3d63422fe
PaigeWw <767427364@qq.com>
2021-10-02 14:07:35 +0800

    fix shop

--- src/controlers/branch.js
+++ src/controlers/branch.js
@@ -21,7 +21,7 @@ export const getList = async (ctx, next) => {
     let { query } = ctx.request;
     query.isDel = 0;
     const resData = [];
-    const data = await Branch.find(query);
+    const data = await Branch.find(query).sort({ createdAt: -1 });
     for (let i = 0; i < data.length; i++) {
       const bk = await BranchKind.find({ isDel: 0, branch: data[i]._id });
       const { createdAt, isDel, namecn, nameen, updatedAt, _id ,description, status} = data[i];
@@ -75,7 +75,7 @@ export const getVisibleList = async (ctx, next) => {
     if (name) {
       q.name = name;
     }
-    let data = await Branch.find(q).sort({ sort: 1 });
+    let data = await Branch.find(q).sort({ createdAt: -1 })
     const user = await getCurrentUser(ctx);
     let result = [];
     let resData = [];
