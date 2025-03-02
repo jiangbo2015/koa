16c8473a5c585021f8707b0505e449c59bd20a8c
jiangbo2015 <772051431@qq.com>
2025-03-02 11:27:25 +0800

    feat: dev

--- hook.js
+++ hook.js
@@ -7,16 +7,12 @@ const webhooks = new WebhooksApi({
 });
 
 webhooks.on("*", ({ id, name, payload }) => {
-  let koaTest = "git pull && yarn && pm2 restart koa-test";
-  let koaProd = "cd ../koa-prod && git pull && yarn && pm2 restart koa-prod";
-  let crm =
-    "cd ../mm-crm && git pull && yarn && yarn build-test && pm2 restart crm";
+  let koaTest = "cd /home/koa-dev && git pull && yarn && pm2 restart koa-test";
+  let koaProd = "cd /home/koa-prod && git pull && yarn && pm2 restart koa-production";
+  let crmTest =
+    "cd /home/mm-crm && git pull && yarn && yarn build-test && pm2 restart crm";
   let crmProd =
-    "cd ../crm-prod && git pull && yarn && yarn build-prod && pm2 restart crm-prod";
-  let miss =
-    "cd ../mrmiss2-dev && git pull && yarn && yarn build-test && pm2 restart miss";
-  let missProd =
-    "cd ../mm-next-prod && git pull && yarn && yarn build && pm2 restart miss-prod";
+    "cd /home/crm-prod && git pull && yarn && yarn build-prod && pm2 restart crm-prod";
   console.log(payload.ref, "ref");
   console.log(id, name);
   // master 分支
@@ -32,16 +28,10 @@ webhooks.on("*", ({ id, name, payload }) => {
       if (payload.ref.includes("master")) {
         exec(crmProd);
       } else {
-        exec(crm);
+        exec(crmTest);
       }
     }
-    if (payload.repository.name === "mrmiss2.0") {
-      if (payload.ref.includes("master")) {
-        exec(missProd);
-      } else {
-        exec(miss);
-      }
-    }
+    
   } catch (err) {
     console.log(err);
   }
